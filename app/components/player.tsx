1bda28d10cd367e110b64af450cceab372df6484
Allen Tran <allentran@allens-macbook.local>
2022-01-30 14:01:38 -0800

    player bar progress

--- app/components/player.tsx
+++ app/components/player.tsx
@@ -11,19 +11,69 @@ import {
   Text,
 } from "@chakra-ui/react";
 import ReactHowler from "react-howler";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   MdShuffle,
   MdSkipPrevious,
   MdSkipNext,
   MdOutlinePlayCircleFilled,
-  MdPauseCircleFilled,
+  MdOutlinePauseCircleFilled,
   MdOutlineRepeat,
 } from "react-icons/md";
 import { useStoreActions } from "easy-peasy";
 
 const Player = () => {
-  return <Box>hello from PlayerBar</Box>;
+  return (
+    <Box>
+      <Box>{/* <ReactHowler /> */}</Box>
+      <Center color="gray.600">
+        <ButtonGroup>
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            icon={<MdShuffle />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="skip"
+            fontSize="24px"
+            icon={<MdSkipPrevious />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="play"
+            fontSize="40px"
+            icon={<MdOutlinePlayCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="pause"
+            fontSize="40px"
+            icon={<MdOutlinePauseCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="skip"
+            fontSize="24px"
+            icon={<MdSkipNext />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="repeat"
+            fontSize="24px"
+            icon={<MdOutlineRepeat />}
+          />
+        </ButtonGroup>
+      </Center>
+    </Box>
+  );
 };
 
 export default Player;
